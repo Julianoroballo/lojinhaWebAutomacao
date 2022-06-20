1bf5dfde2de52f0fbd55e208d136bc8d48cd4ed8
Juliano Albuquerque <julianoroballo@gmail.com>
2022-06-19 22:18:37 -0300

    Initial commit

---
+++ src/test/java/modulos/produtos/ProdutosTest.java
@@ -0,0 +1,27 @@
+package modulos.produtos;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Teste web domodulo de Produttos")
+public class ProdutosTest {
+    @Test
+    @DisplayName("não e permitido registrar um produto com valor igual a Zero")
+    public void testNaoEPermitidoRegistrarProdutoComValorIgualAZero(){
+        //abrir o navegador
+
+        //navegar para a apagina da Lojinha Web
+
+        //fazer login
+
+        //vou para tela de registro do produto
+
+        //vou preencher dados do produto e o valor será igual a zero
+
+        //vou submeter o formulario
+
+        //vou validar que a mensagem de erro que foi apresentada 
+    }
+
+
+}
